6540c40a7efc63405bc41b8dae6e7239e979b11b
Ven <ven.wu@baifu-tech.net>
2024-09-25 16:59:16 +0800

    [Feature] 支援 Linux 編譯

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MD5",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +16,16 @@ let package = Package(
             targets: ["MD5"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MD5",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+            ]),
         .testTarget(
             name: "MD5Tests",
             dependencies: ["MD5"]),
--- Sources/MD5/MD5.swift
+++ Sources/MD5/MD5.swift
@@ -1,23 +1,9 @@
 import Foundation
-import var CommonCrypto.CC_MD5_DIGEST_LENGTH
-import func CommonCrypto.CC_MD5
-import typealias CommonCrypto.CC_LONG
+import Crypto
 
 public extension Data {
     var md5Data: Data {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digestData = Data(count: length)
-
-        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-            self.withUnsafeBytes { messageBytes -> UInt8 in
-                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                    let messageLength = CC_LONG(self.count)
-                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
-                }
-                return 0
-            }
-        }
-        return digestData
+        Data(Insecure.MD5.hash(data: self))
     }
     
     var md5: String {
